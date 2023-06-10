62d1e5a3d2f466e0f1c22b22a2d8b32377167a7e
M. Alexander Díaz <mario.diaz@correounivalle.edu.co>
2023-06-10 18:16:03 -0500

    little changes, this file should be removed in the future

--- pvs.py
+++ pvs.py
@@ -1,9 +1,9 @@
 from pvsCreator import PVSCreator
 import numpy as np
 
-NVox = np.array([16, 16, 16])
+NVox = np.array([8, 8, 8])
 
 pvs = PVSCreator()
 pvs.createPVS2(NVox)
 # pvs.rotatePVS()
-pvs.plotPVS2()
+pvs.plotPVS()
