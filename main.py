d5c5f328b6924c76a7af9df6fe7815936c42e10e
M. Alexander Díaz <mario.diaz@correounivalle.edu.co>
2023-06-22 15:58:33 -0500

    Documentation and a little refactoring

--- main.py
+++ main.py
@@ -3,20 +3,29 @@ import sys
 import glob
 from pvsCreator import PVSAdder
 
-# specify the folder path where the files are located
-folder_path = "training_segs/"
-result_dir = "pvs_training_segs/"
+"""
+Parameters:
+    - num_imgs: number of images to generate
+    - training_dir: directory where the images are stored
+    - result_dir: directory where the images will be stored
+
+Returns:
+    - num_imgs images with the PVS added to the ROIs
+
+Example:
+    python main.py 1000 training_segs/ ~/pvs_labels/
+"""
 
 num_imgs = int(sys.argv[1])
+training_dir = sys.argv[2]
+result_dir = sys.argv[3]
 
-# get a list of all the files in the folder
-file_list = glob.glob(os.path.join(folder_path, "*"))
+if not os.path.exists(result_dir):
+    os.makedirs(result_dir)
 
-# loop through each file and apply my_method from MyClass to it
-k = 0
+file_list = glob.glob(os.path.join(training_dir, "*"))
+
 for i in range(num_imgs):
     file = file_list[i % len(file_list)]
     mask = PVSAdder()
-    mask.addPVS(file, result_dir, k)
-    k += 1
-
+    mask.addPVS(file, result_dir, i)
